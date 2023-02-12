dd3b65926abfce2cf6e6fdc647ba14f5d9398c82
RaggedyRach <118667330+RaggedyRach@users.noreply.github.com>
2023-02-12 12:39:46 +0000

    ifandswitchinreportbuilder

---
+++ OTBIS.Web/Services/MyStateContainer3.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using OTBIS.Web.Models;
+
+namespace OTBIS.Web.Services
+{
+    public class MyStateContainer3
+
+    {
+        public MyStateContainer3() { }   
+        public ModelList Value { get; set; }
+
+        public event Action OnStateChange;
+
+        public void SetValue(ModelList value)
+        {
+            this.Value = value;
+            NotifyStateChanged();
+        }
+
+        private void NotifyStateChanged() => OnStateChange?.Invoke();
+
+    }
+   
+}
