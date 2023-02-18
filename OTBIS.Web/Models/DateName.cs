da826722bee838678c28bb063035e1f261904afe
RaggedyRach <118667330+RaggedyRach@users.noreply.github.com>
2023-02-18 21:08:47 +0000

    visual improvement for multiple dates report build

---
+++ OTBIS.Web/Models/DateName.cs
@@ -0,0 +1,13 @@
+﻿namespace OTBIS.Web.Models
+{
+    public class DateName
+
+    {
+        public string name { get; set; }
+
+        public DayOfWeek dayOfWeek { get; set; }
+        
+    }
+
+   
+}
--- OTBIS.Web/Models/ModelList.cs
+++ OTBIS.Web/Models/ModelList.cs
@@ -5,16 +5,17 @@ namespace OTBIS.Web.Models
     public class ModelList
     {
 
-        public List<TransByDate> transByDatesList { get; set; } 
+        public List<TransByDate>? transByDatesList { get; set; } 
 
-        public List<TransByMonth>  transByMonthList { get; set;}
+        public List<TransByMonth>?  transByMonthList { get; set;}
 
-        public List<TransByDept> transByDeptList { get;set; }
+        public List<TransByDept>? transByDeptList { get;set; }
 
-        public List<TransByCat> transByCatList { get; set; }
-        public List<TransBySubCat> transBySubCatList { get; set; }
-        public List<TransByItem> transByItemList { get; set; }
+        public List<TransByCat>? transByCatList { get; set; }
+        public List<TransBySubCat>? transBySubCatList { get; set; }
+        public List<TransByItem>? transByItemList { get; set; }
 
+        public List<TransByTill>? transByTillList { get; set; }
 
     }
 }
